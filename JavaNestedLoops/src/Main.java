23e341f8e32eea8d6534bd40fd673def4f1cf57f
MykolaDekusar <ndekusar97@gmail.com>
2024-10-27 14:16:44 +0100

    Ask user for row and column input to print a figure using nested for loops

--- JavaNestedLoops/src/Main.java
+++ JavaNestedLoops/src/Main.java
@@ -1,9 +1,26 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	//I nested loops sono dei loop dentro and altri loop tipo un for dentro un altro for
+	Scanner scanner = new Scanner(System.in);
+	//chiediamo all'utente il numero di colonne e di righe
+	int rows;
+	int col;
+	String symbol="";
+	System.out.println("Inserisci il numero di righe");
+	rows=scanner.nextInt();
+	System.out.println("Inserisci il numero di colonne");
+	col=scanner.nextInt();
+	System.out.println("Inserisci il simbolo che vuoi usare per la forma");
+	symbol=scanner.next();
+	for(int i=0;i<rows;i++) {
+		System.out.println();//mi serve per andare a capo ad ogni riga
+		for(int j=0;j<col;j++) {
+			System.out.print(symbol);
+		}
+		
+	}
 	}
 
 }
