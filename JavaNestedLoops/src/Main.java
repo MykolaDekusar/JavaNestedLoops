5e16cb62b1cea212e34e5a179d07346cc91b05fa
MykolaDekusar <ndekusar97@gmail.com>
2024-10-27 14:23:22 +0100

    Closure of scanner

--- JavaNestedLoops/src/Main.java
+++ JavaNestedLoops/src/Main.java
@@ -3,24 +3,25 @@ public class Main {
 
 	public static void main(String[] args) {
 	//I nested loops sono dei loop dentro and altri loop tipo un for dentro un altro for
-	Scanner scanner = new Scanner(System.in);
-	//chiediamo all'utente il numero di colonne e di righe
-	int rows;
-	int col;
-	String symbol="";
-	System.out.println("Inserisci il numero di righe");
-	rows=scanner.nextInt();
-	System.out.println("Inserisci il numero di colonne");
-	col=scanner.nextInt();
-	System.out.println("Inserisci il simbolo che vuoi usare per la forma");
-	symbol=scanner.next();
-	for(int i=0;i<rows;i++) {
-		System.out.println();//mi serve per andare a capo ad ogni riga
-		for(int j=0;j<col;j++) {
-			System.out.print(symbol);
+		Scanner scanner = new Scanner(System.in);
+		//chiediamo all'utente il numero di colonne e di righe
+		int rows;
+		int col;
+		String symbol="";
+		System.out.println("Inserisci il numero di righe");
+		rows=scanner.nextInt();
+		System.out.println("Inserisci il numero di colonne");
+		col=scanner.nextInt();
+		System.out.println("Inserisci il simbolo che vuoi usare per la forma");
+		symbol=scanner.next();
+		for(int i=0;i<rows;i++) {
+			System.out.println();//mi serve per andare a capo ad ogni riga
+			for(int j=0;j<col;j++) {
+				System.out.print(symbol);
+			}
+			
 		}
-		
-	}
+		scanner.close();
 	}
 
 }
